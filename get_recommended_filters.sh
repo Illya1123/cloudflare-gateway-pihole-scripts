a8f5e29a0420d1d73ab9ba353be9ccabd8b92878
Lê Quốc Anh <21520565@gm.uit.edu.vn>
2024-06-05 22:11:16 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -5,12 +5,12 @@ source $(dirname "$0")/lib/helpers.sh
 # declare an array of urls
 urls=(
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
-  #https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
-  #https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+  https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
   https://adaway.org/hosts.txt
-  #https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
-  #https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
+  https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+  https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
   https://abpvn.com/android/abpvn.txt
   https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardDNS.txt
   https://github.com/bigdargon/hostsVN/raw/master/hosts
