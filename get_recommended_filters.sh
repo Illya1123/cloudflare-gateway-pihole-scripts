2fe74d9fad123e8ecca707134b2b6430dcbaf62f
Lê Quốc Anh <21520565@gm.uit.edu.vn>
2024-06-05 22:00:17 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -4,9 +4,9 @@ source $(dirname "$0")/lib/helpers.sh
 
 # declare an array of urls
 urls=(
-  #https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
   #https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
-  #https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
+  https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
   #https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
   https://adaway.org/hosts.txt
   #https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
@@ -14,7 +14,7 @@ urls=(
   https://abpvn.com/android/abpvn.txt
   https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardDNS.txt
   https://github.com/bigdargon/hostsVN/raw/master/hosts
-  #https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
+  https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
 )
 
 # download all files in parallel and append them to input.csv
