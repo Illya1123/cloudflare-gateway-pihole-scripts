e0c1c30818b6ec0989f9ad593ef2b730371bd95a
Lê Quốc Anh <21520565@gm.uit.edu.vn>
2024-06-05 22:13:56 +0700

    Update get_recommended_filters.sh

--- get_recommended_filters.sh
+++ get_recommended_filters.sh
@@ -4,14 +4,14 @@ source $(dirname "$0")/lib/helpers.sh
 
 # declare an array of urls
 urls=(
+  https://abpvn.com/android/abpvn.txt
+  https://adaway.org/hosts.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_adblock.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_gambling.txt
   https://raw.githubusercontent.com/mullvad/dns-blocklists/main/output/doh/doh_privacy.txt
   https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
-  https://adaway.org/hosts.txt
   https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
   https://raw.githubusercontent.com/bigdargon/hostsVN/master/option/domain.txt
-  https://abpvn.com/android/abpvn.txt
   https://raw.githubusercontent.com/r-a-y/mobile-hosts/master/AdguardDNS.txt
   https://github.com/bigdargon/hostsVN/raw/master/hosts
   https://adguardteam.github.io/AdGuardSDNSFilter/Filters/filter.txt
